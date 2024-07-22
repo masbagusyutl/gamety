c14833159385a331b2e0505ee023475ed3cec705
masbagusyutl <82576353+masbagusyutl@users.noreply.github.com>
2024-07-22 11:50:09 +0700

    Update g.py

--- g.py
+++ g.py
@@ -1,34 +1,12 @@
 import requests
 import json
-import time
 import random
+import time
 from datetime import datetime, timedelta
-from urllib.parse import quote
 
-def load_tgwebappdata(file_path):
+def load_authorizations(file_path):
     with open(file_path, 'r') as f:
         return [line.strip() for line in f]
-
-def format_tgwebappdata(raw_data):
-    base_url = 'https://dt49tlqaucy6e.cloudfront.net/Prod_Build/index.html?bot_username=GametyApp_bot#tgWebAppData='
-    encoded_data = quote(raw_data)
-    tgwebappdata_url = f"{base_url}{encoded_data}&tgWebAppVersion=7.6&tgWebAppPlatform=tdesktop&tgWebAppThemeParams=%7B\"accent_text_color\"%3A\"%236ab2f2\"%2C\"bg_color\"%3A\"%2317212b\"%2C\"button_color\"%3A\"%235288c1\"%2C\"button_text_color\"%3A\"%23ffffff\"%2C\"destructive_text_color\"%3A\"%23ec3942\"%2C\"header_bg_color\"%3A\"%2317212b\"%2C\"hint_color\"%3A\"%23708499\"%2C\"link_color\"%3A\"%236ab3f3\"%2C\"secondary_bg_color\"%3A\"%23232e3c\"%2C\"section_bg_color\"%3A\"%2317212b\"%2C\"section_header_text_color\"%3A\"%236ab3f3\"%2C\"section_separator_color\"%3A\"%23111921\"%2C\"subtitle_text_color\"%3A\"%23708499\"%2C\"text_color\"%3A\"%23f5f5f5\"%7D"
-    return tgwebappdata_url
-
-def get_authorization(tgwebappdata_url):
-    try:
-        response = requests.get(tgwebappdata_url)
-        response.raise_for_status()  # Raise an error for bad status codes
-        auth_data = response.json()
-        return auth_data.get('authorization')
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except requests.exceptions.RequestException as req_err:
-        print(f"Request error occurred: {req_err}")
-    except json.decoder.JSONDecodeError as json_err:
-        print(f"JSON decode error: {json_err}")
-        print(f"Response text: {response.text}")
-    return None
 
 def get_current_time():
     return datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
@@ -102,18 +80,12 @@ def countdown_and_restart():
 def main():
     click_url = 'https://gamety-clicker-api.metafighter.com/api/v1/actions/click/'
     defeat_url = 'https://gamety-clicker-api.metafighter.com/api/v1/actions/defeat/'
-    tgwebappdata_list = load_tgwebappdata('data.txt')
-    num_accounts = len(tgwebappdata_list)
-    for i, tgwebappdata in enumerate(tgwebappdata_list):
+    auth_tokens = load_authorizations('data.txt')
+    num_accounts = len(auth_tokens)
+    for i, auth_token in enumerate(auth_tokens):
         print(f"Processing account {i+1}/{num_accounts}")
-        tgwebappdata_url = format_tgwebappdata(tgwebappdata)
-        print(f"Formatted URL: {tgwebappdata_url}")  # Debugging: print the formatted URL
-        auth_token = get_authorization(tgwebappdata_url)
-        if auth_token:
-            perform_clicks(auth_token, click_url, defeat_url)
-            print(f"Finished processing account {i+1}/{num_accounts}")
-        else:
-            print(f"Skipping account {i+1}/{num_accounts} due to authorization failure.")
+        perform_clicks(auth_token, click_url, defeat_url)
+        print(f"Finished processing account {i+1}/{num_accounts}")
         time.sleep(5)  # Delay between accounts
     countdown_and_restart()
 
